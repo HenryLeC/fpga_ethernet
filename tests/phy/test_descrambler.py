6228fb732cd304dd062567eab2175e3c2417c49d
Henry LeCompte <henry@lunar-glass.com>
2026-04-22 21:30:22 -0400

    FEAT: Add descrambler and accompanying tests

--- tests/phy/test_descrambler.py
+++ tests/phy/test_descrambler.py
@@ -5,14 +5,14 @@ from cocotb_tools.runner import get_runner
 from cocotb.triggers import Timer
 
 
-def test_scrambler_runner():
+def test_descrambler_runner():
     proj_path = Path(__file__).resolve().parent.parent.parent
-    sources = [proj_path / "src" / "phy" / "pcs" / "scrambler.sv"]
+    sources = [proj_path / "src" / "phy" / "pcs" / "descrambler.sv"]
 
     runner = get_runner("verilator")
     runner.build(
         sources=sources,
-        hdl_toplevel="scrambler",
+        hdl_toplevel="descrambler",
         always=True,
         waves=True,
         build_args=[
@@ -21,19 +21,19 @@ def test_scrambler_runner():
         ],
     )
     runner.test(
-        hdl_toplevel="scrambler",
+        hdl_toplevel="descrambler",
         test_module=__name__,
         waves=True,
-        test_args=["--trace-file", "../scrambler.fst"],
+        test_args=["--trace-file", "../descrambler.fst"],
     )
 
 
 @cocotb.test()
-async def test_scrambler(dut):
+async def test_descrambler(dut):
 
     # These come from https://grouper.ieee.org/groups/802/3/ae/public/jul00/walker_1_0700.pdf
-    data_in = 0x000000000000001E
-    data_correct = 0x7BFFF0800000001E
+    data_in = 0x7BFFF0800000001E
+    data_correct = 0x000000000000001E
 
     dut.data_in.value = data_in
     dut.lfsr_state_in.value = (1 << 58) - 1
@@ -46,8 +46,8 @@ async def test_scrambler(dut):
     print(hex(data_out))
     assert data_out == data_correct
 
-    data_in = 0xD555555555555578
-    data_correct = 0x623016AAAAAD1578
+    data_in = 0x623016AAAAAD1578
+    data_correct = 0xD555555555555578
 
     dut.data_in.value = data_in
     dut.lfsr_state_in.value = state_out
