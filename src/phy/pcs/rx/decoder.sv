c9cff4d0542d6e77771a31a1fa8be3ebe60d473a
Henry LeCompte <henry@lunar-glass.com>
2026-05-14 19:31:41 -0400

    FEAT: Add all block types to decoder

--- src/phy/pcs/rx/decoder.sv
+++ src/phy/pcs/rx/decoder.sv
@@ -38,10 +38,65 @@ module decoder #() (
                     xgmii_rx_data = {rx_data[63:40], RS_START, {4{RS_IDLE}}};
                     xgmii_rx_control = 8'h1F;
                 end
+            BT_O0S4:
+                begin
+                    xgmii_rx_data = {rx_data[63:40], RS_START, rx_data[31:8], RS_SEQ};
+                    xgmii_rx_control = 8'h11;
+                end
+            BT_O0O4:
+                begin
+                    xgmii_rx_data = {rx_data[63:40], RS_SEQ, rx_data[31:8], RS_SEQ};
+                    xgmii_rx_control = 8'h11;
+                end
             BT_S0:
                 begin
                     xgmii_rx_data = {rx_data[63:8], RS_START};
                     xgmii_rx_control = 8'h01;
+                end
+            BT_O0:
+                begin
+                    xgmii_rx_data = {{4{RS_IDLE}}, rx_data[31:8], RS_SEQ};
+                    xgmii_rx_control = 8'hF1;
+                end
+            BT_T0:
+                begin
+                    xgmii_rx_data = {{7{RS_IDLE}}, RS_TERM};
+                    xgmii_rx_control = 8'hFF;
+                end
+            BT_T1:
+                begin
+                    xgmii_rx_data = {{6{RS_IDLE}}, RS_TERM, rx_data[7:0]};
+                    xgmii_rx_control = 8'hFE;
+                end
+            BT_T2:
+                begin
+                    xgmii_rx_data = {{5{RS_IDLE}}, RS_TERM, rx_data[15:0]};
+                    xgmii_rx_control = 8'hFC;
+                end
+            BT_T3:
+                begin
+                    xgmii_rx_data = {{4{RS_IDLE}}, RS_TERM, rx_data[23:0]};
+                    xgmii_rx_control = 8'hF8;
+                end
+            BT_T4:
+                begin
+                    xgmii_rx_data = {{3{RS_IDLE}}, RS_TERM, rx_data[31:0]};
+                    xgmii_rx_control = 8'hF0;
+                end
+            BT_T5:
+                begin
+                    xgmii_rx_data = {{2{RS_IDLE}}, RS_TERM, rx_data[39:0]};
+                    xgmii_rx_control = 8'hE0;
+                end
+            BT_T6:
+                begin
+                    xgmii_rx_data = {RS_IDLE, RS_TERM, rx_data[47:0]};
+                    xgmii_rx_control = 8'hC0;
+                end
+            BT_T7:
+                begin
+                    xgmii_rx_data = {RS_TERM, rx_data[55:0]};
+                    xgmii_rx_control = 8'h80;
                 end
             default:
                 begin
