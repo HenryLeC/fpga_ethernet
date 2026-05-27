56517c07c866e0a7164146975cc1d28ace067a89
Henry LeCompte <henry@lunar-glass.com>
2026-05-27 15:36:36 -0400

    FEAT: Move RX side PCS to 32 bit interfaces

--- src/phy/pcs/rx/pcs_rx.sv
+++ src/phy/pcs/rx/pcs_rx.sv
@@ -1,41 +1,85 @@
 `default_nettype none
 module pcs_rx #(
-    parameter LFSR_INITIAL_STATE = 58'hFEEDFEEDFEEDFEE
+    parameter bit [57:0] LFSR_INITIAL_STATE = 58'hFEEDFEEDFEEDFEE
 ) (
     input wire clk,
     
     // XGMII interface
-    output wire [63:0] RXD,
-    output wire [7:0] RXC,
+    output reg [31:0] RXD,
+    output reg [3:0] RXC,
 
-    input wire [63:0] rx_data,
-    input wire [1:0] rx_header
+    input wire [31:0] rx_data,
+    input wire [1:0]  rx_header,
+    input wire        header_valid
 );
+
+    reg [1:0] header;
+    reg [31:0] prev_block;
+
+    wire [63:0] full_block = {rx_data, prev_block};
+    reg         full_valid;
+
+
+    initial full_valid = 0;
+    always_ff @(posedge clk) begin
+        prev_block <= rx_data;
+
+        if (header_valid) begin
+            header <= rx_header;
+        end
+        full_valid <= header_valid;
+    end
 
     wire [63:0] unscrambled_data;
 
-    reg [57:0] lfsr_state = LFSR_INITIAL_STATE;
-
-    decoder decoder_inst(
-        .clk(clk),
-
-        .RXD(RXD),
-        .RXC(RXC),
-
-        .rx_header(rx_header),
-        .rx_data(unscrambled_data)
-    );
-
+    reg  [57:0] lfsr_state = LFSR_INITIAL_STATE;
     wire [57:0] next_lfsr_state;
 
     descrambler descrambler_inst(
-        .data_in(rx_data),
+        .data_in(full_block),
         .lfsr_state_in(lfsr_state),
         .data_out(unscrambled_data),
         .lfsr_state_out(next_lfsr_state)
     );
 
     always_ff @(posedge clk) begin: store_lfsr_state
-        lfsr_state <= next_lfsr_state;
+        if (full_valid)
+            lfsr_state <= next_lfsr_state;
     end
+
+    wire [63:0] RXD_full;
+    wire [7:0]  RXC_full;
+    reg         RXX_valid;
+
+    reg [31:0] RXD_upper;
+    reg [3:0]  RXC_upper;
+
+    decoder decoder_inst(
+        .clk(clk),
+
+        .RXD(RXD_full),
+        .RXC(RXC_full),
+
+        .rx_header(rx_header),
+        .rx_data(unscrambled_data)
+    );
+
+    always_ff @(posedge clk) begin
+        RXX_valid <= full_valid;
+        if (RXX_valid) begin
+            RXD_upper <= RXD_full[63:32];
+            RXC_upper <= RXC_full[7:4];
+        end
+    end
+
+    always_comb begin: XGMII_switch_word
+        if (RXX_valid) begin
+            RXD = RXD_full[31:0];
+            RXC = RXC_full[3:0];
+        end else begin
+            RXD = RXD_upper;
+            RXC = RXC_upper;
+        end
+    end
+
 endmodule
---
+++ tests/phy/test_pcs_rx.py
@@ -0,0 +1,84 @@
+from pathlib import Path
+
+import cocotb
+from cocotb_tools.runner import get_runner
+from cocotb.types import LogicArray
+from cocotb.triggers import FallingEdge, Timer
+
+
+def test_pcs_rx_runner():
+    proj_path = Path(__file__).resolve().parent.parent.parent
+    sources = [proj_path / "src" / "phy" / "pcs" / "rx" / "pcs_rx.sv"]
+
+    runner = get_runner("verilator")
+    runner.build(
+        sources=sources,
+        hdl_toplevel="pcs_rx",
+        parameters={"LFSR_INITIAL_STATE": "58'hFFFFFFFFFFFFFFF"},
+        always=True,
+        waves=True,
+        build_args=[
+            "--trace-fst",
+            "--trace-structs",
+            f"-I{proj_path / "src"}",
+            f"-I{proj_path / "src" / "include"}",
+            f"-I{proj_path / "src" / "phy" / "pcs" / "rx"}",
+        ],
+    )
+    runner.test(
+        hdl_toplevel="pcs_rx",
+        test_module=__name__,
+        waves=True,
+        test_args=["--trace-file", "../pcs_rx.fst"],
+    )
+
+
+async def generate_clock(dut):
+    """Generate clock pulses."""
+
+    while True:
+        dut.clk.value = 0
+        await Timer(1, unit="ns")
+        dut.clk.value = 1
+        await Timer(1, unit="ns")
+
+
+@cocotb.test()
+async def test_pcs_rx(dut):
+
+    cocotb.start_soon(generate_clock(dut))  # run the clock "in the background"
+
+    await Timer(5, unit="ns")  # wait a bit
+    await FallingEdge(dut.clk)  # wait for falling edge/"negedge"
+
+    # These come from https://grouper.ieee.org/groups/802/3/ae/public/jul00/walker_1_0700.pdf
+    data_in = [0x0000001E, 0x7BFFF080, 0xAAAD1578, 0x623016AA]
+    header_in = [0b01, 0b01]
+    RXD_out = [0x07070707, 0x07070707, 0x555555FB, 0xD5555555]
+    RXC_out = [0xF, 0xF, 0x1, 0x0]
+
+    for i in range(len(header_in)):
+        if i != 0:
+            assert dut.RXC.value == RXC_out[(i - 1) * 2]
+            assert dut.RXD.value == RXD_out[(i - 1) * 2]
+        dut.header_valid.value = 1
+        dut.rx_header.value = header_in[i]
+        dut.rx_data.value = data_in[2 * i]
+
+        await FallingEdge(dut.clk)
+        if i != 0:
+            assert dut.RXC.value == RXC_out[(i - 1) * 2 + 1]
+            assert dut.RXD.value == RXD_out[(i - 1) * 2 + 1]
+        dut.header_valid.value = 0
+        dut.rx_header.value = 0
+        dut.rx_data.value = data_in[2 * i + 1]
+
+        await FallingEdge(dut.clk)
+
+    assert dut.RXC.value == RXC_out[-2]
+    assert dut.RXD.value == RXD_out[-2]
+
+    await FallingEdge(dut.clk)
+
+    assert dut.RXC.value == RXC_out[-1]
+    assert dut.RXD.value == RXD_out[-1]
