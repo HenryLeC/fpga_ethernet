71517edcc80b29cf51f045febc895baa68028376
Henry LeCompte <henry@lunar-glass.com>
2026-05-29 09:57:21 -0400

    FEAT: Add CRC files to vivado project

---
+++ Top/eth_controller/post-creation.tcl
@@ -0,0 +1,4 @@
+set_property file_type {Memory Initialization Files} [get_files  crc_lookup_0.hex]
+set_property file_type {Memory Initialization Files} [get_files  crc_lookup_1.hex]
+set_property file_type {Memory Initialization Files} [get_files  crc_lookup_2.hex]
+set_property file_type {Memory Initialization Files} [get_files  crc_lookup_3.hex]
