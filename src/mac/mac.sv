8c73bb3ee2aad30832b2994d5dd24f926865ea73
Henry LeCompte <henry@lunar-glass.com>
2026-06-07 10:02:26 -0400

    FEAT: Add ipv4 header sending to mac

--- src/mac/mac.sv
+++ src/mac/mac.sv
@@ -34,13 +34,39 @@ module mac (
         .probe4(o_empty)
     );
 
+    wire valid, last, ready;
+    wire [31:0] data;
 
-    dummy_frame_encoder frame_gen_inst (
+
+    frame_encoder frame_encoder_inst (
         .i_clk(i_txclk),
         .i_arst(i_arst),
 
+        .i_clientdata(data),
+        .i_clientdata_valid({4{valid}}),
+        .i_last(last),
+        .o_ready(ready),
+
         .TXC(o_TXC),
         .TXD(o_TXD)
+    );
+
+    ipv4_header_encode header_generator (
+        .i_clk(i_txclk),
+        .i_arst(i_arst),
+        
+        .m_axis_tvalid(valid),
+        .m_axis_tdata(data),
+        .m_axis_tready(ready),
+        .m_axis_tlast(last),
+
+        .packet_length(16'd20),
+        .identification(16'h1234),
+        .protocol(8'd17),
+        .source_address(32'h01010101),
+        .destination_address(32'h0A0001FF),
+        .data_valid(1)
+
     );
 
 endmodule
